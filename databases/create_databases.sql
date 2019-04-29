d41f2e584f7b257c88b3128b4e417330227e5755
Yogesh Rathor - (Digital) <YO20077673@wipro.com>
2019-04-29 14:26:09 +0530

    Distributed application

--- databases/create_databases.sql
+++ databases/create_databases.sql
@@ -1,21 +1,21 @@
-DROP DATABASE IF EXISTS tracker_allocations_dev;
-DROP DATABASE IF EXISTS tracker_backlog_dev;
-DROP DATABASE IF EXISTS tracker_registration_dev;
-DROP DATABASE IF EXISTS tracker_timesheets_dev;
-DROP DATABASE IF EXISTS tracker_allocations_test;
-DROP DATABASE IF EXISTS tracker_backlog_test;
-DROP DATABASE IF EXISTS tracker_registration_test;
-DROP DATABASE IF EXISTS tracker_timesheets_test;
+DROP DATABASE IF EXISTS tracker_allocations_dev_yrathor;
+DROP DATABASE IF EXISTS tracker_backlog_dev_yrathor;
+DROP DATABASE IF EXISTS tracker_registration_dev_yrathor;
+DROP DATABASE IF EXISTS tracker_timesheets_dev_yrathor;
+DROP DATABASE IF EXISTS tracker_allocations_test_yrathor;
+DROP DATABASE IF EXISTS tracker_backlog_test_yrathor;
+DROP DATABASE IF EXISTS tracker_registration_test_yrathor;
+DROP DATABASE IF EXISTS tracker_timesheets_test_yrathor;
 
 CREATE USER IF NOT EXISTS 'tracker'@'localhost'
   identified by '';
 GRANT ALL PRIVILEGES ON *.* TO 'tracker' @'localhost';
 
-CREATE DATABASE tracker_allocations_dev;
-CREATE DATABASE tracker_backlog_dev;
-CREATE DATABASE tracker_registration_dev;
-CREATE DATABASE tracker_timesheets_dev;
-CREATE DATABASE tracker_allocations_test;
-CREATE DATABASE tracker_backlog_test;
-CREATE DATABASE tracker_registration_test;
-CREATE DATABASE tracker_timesheets_test;
+CREATE DATABASE tracker_allocations_dev_yrathor;
+CREATE DATABASE tracker_backlog_dev_yrathor;
+CREATE DATABASE tracker_registration_dev_yrathor;
+CREATE DATABASE tracker_timesheets_dev_yrathor;
+CREATE DATABASE tracker_allocations_test_yrathor;
+CREATE DATABASE tracker_backlog_test_yrathor;
+CREATE DATABASE tracker_registration_test_yrathor;
+CREATE DATABASE tracker_timesheets_test_yrathor;
